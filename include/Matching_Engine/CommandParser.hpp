5c3849139b573be6b60b12f18213abb5acd64597
bhupendraramgade5 <bhupendraramgade5@gmail.com>
2026-04-03 13:22:18 +0000

    Command Parser

---
+++ include/Matching_Engine/CommandParser.hpp
@@ -0,0 +1,26 @@
+#ifndef COMMAND_PARSER_HPP
+#define COMMAND_PARSER_HPP
+
+#include "Command.hpp"
+
+#include <string>
+#include <optional>
+
+class CommandParser
+{
+public:
+
+    std::optional<Command> parse(const std::string& input);
+
+private:
+
+    Command parseNew(std::istringstream& iss);
+
+    Command parseCancel(std::istringstream& iss);
+
+    Command parseModify(std::istringstream& iss);
+
+    Command parsePrint(std::istringstream& iss);
+};
+
+#endif // COMMAND_PARSER_HPP
---
+++ src/Matching_Engine/Command_Parser.cpp
@@ -0,0 +1,107 @@
+#include "CommandParser.hpp"
+
+#include <sstream>
+#include <iostream>
+
+std::optional<Command> CommandParser::parse(const std::string& input)
+{
+    std::istringstream iss(input);
+    std::string commandType;
+
+    if (!(iss >> commandType))
+    {
+        return std::nullopt;
+    }
+
+    if (commandType == "NEW")
+    {
+        return parseNew(iss);
+    }
+    else if (commandType == "CANCEL")
+    {
+        return parseCancel(iss);
+    }
+    else if (commandType == "MODIFY")
+    {
+        return parseModify(iss);
+    }
+    else if (commandType == "PRINT")
+    {
+        return parsePrint(iss);
+    }
+    else if (commandType == "PRINTALL")
+    {
+        return PrintAllCommand{};
+    }
+
+    return std::nullopt;
+}
+
+
+Command CommandParser::parseNew(std::istringstream& iss)
+{
+    Symbol symbol;
+    OrderId orderId;
+    std::string sideStr;
+    Price price;
+    Quantity qty;
+
+    iss >> symbol >> orderId >> sideStr >> price >> qty;
+
+    Side side = (sideStr == "BUY") ? Side::BUY : Side::SELL;
+
+    Order order{
+        symbol,
+        orderId,
+        0, // userId (future)
+        side,
+        price,
+        qty,
+        0 // timestamp (set later)
+    };
+
+    return NewOrderCommand{order};
+}
+
+
+Command CommandParser::parseCancel(std::istringstream& iss)
+{
+    Symbol symbol;
+    OrderId orderId;
+
+    iss >> symbol >> orderId;
+
+    return CancelOrderCommand{
+        symbol,
+        orderId
+    };
+}
+
+Command CommandParser::parseModify(std::istringstream& iss)
+{
+    Symbol symbol;
+    OrderId orderId;
+    Price price;
+    Quantity qty;
+
+    iss >> symbol >> orderId >> price >> qty;
+
+    return ModifyOrderCommand{
+        symbol,
+        orderId,
+        price,
+        qty
+    };
+}
+
+Command CommandParser::parsePrint(std::istringstream& iss)
+{
+    Symbol symbol;
+
+    iss >> symbol;
+
+    return PrintCommand{
+        symbol
+    };
+}
+
